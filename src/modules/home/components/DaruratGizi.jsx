7c42df9c80ed58bf6f8318a337b77dded4ffd1d2
faracutrisfa <faracutrisfa@gmail.com>
2025-04-22 11:54:11 +0700

    feat: add asset n card to darurat gizi component

--- src/modules/home/components/DaruratGizi.jsx
+++ src/modules/home/components/DaruratGizi.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
+import image1 from '../../../assets/darurat_image1.webp';
+import image2 from '../../../assets/darurat_image2.webp';
+import image3 from '../../../assets/darurat_image3.webp';
+import image4 from '../../../assets/darurat_image4.webp';
 
 const cardData = [
   {
+    image: image1,
     jumlah: '280,000',
     desc: 'Bayi lahir dengan berat yang kurang indikator kurang gizi',
   },
   {
+    image: image2,
     jumlah: '1 dari 5',
     desc: 'Anak dibawah 5 tahun terkena stunting dan terlalu pendek dari seumurannya',
   },
   {
+    image: image3,
     jumlah: '1 dari 12',
     desc: 'Anak-anak di bawah usia lima tahun mengalami gizi buruk, atau terlalu kurus untuk tinggi badan mereka',
   },
   {
+    image: image4,
     jumlah: '2 dari 5',
     desc: 'Anak dibawah usia lima tahun tidak menerima makanan dan minuman yang direkomendasikan',
   },
@@ -40,7 +48,10 @@ const DaruratGizi = () => {
                 key={index}
                 className={`${bgColor} p-3 lg:p-6 rounded-3xl shadow-lg text-center flex flex-col items-center h-full min-h-[250px]`}
               >
-                <div key={index} className={`${bgImage} w-20 h-20 lg:w-24 lg:h-24 rounded-full mb-4`} />
+                <div key={index} className={`${bgImage} flex items-center justify-center w-20 h-20 lg:w-24 lg:h-24 rounded-full mb-4`}>
+                  <img src={card.image} alt="icon" className="w-14 lg:w-16 object-cover" />
+                </div>
+
                 <h2 className="text-primary-100 font-extrabold text-xl lg:text-4xl lg:pt-2">
                   {card.jumlah}
                 </h2>
